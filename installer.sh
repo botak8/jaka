fb0cd0de60869d8dfa660580f4166f858dbbc3e8
Andri Supriatna <139709908+driwwvpn@users.noreply.github.com>
2023-08-09 22:55:38 +0700

    x

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/givpn/bot_panel/master/access > /root/tmp
+    curl -sS https://raw.githubusercontent.com/driwwvpn/bot_panel/master/access > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -23,7 +23,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/givpn/bot_panel/master/access | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/driwwvpn/bot_panel/master/access | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -40,7 +40,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/givpn/bot_panel/master/access | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/driwwvpn/bot_panel/master/access | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -118,17 +118,17 @@ dircreate
         read adm_ids
         echo "Admin_ID=$adm_ids" >> /root/ResBotAuth
         }
-        echo -ne "Username admin panel use '@' [Ex: @givpn] : "
+        echo -ne "Username admin panel use '@' [Ex: @driwwvpn] : "
         read admin_pnl
-        [[ -z $admin_pnl ]] && admin_pnl="@givpn"
+        [[ -z $admin_pnl ]] && admin_pnl="@driwwvpn"
         echo ""
         echo -ne "Limit trial for reseller create user trial [default:1] : "
         read limit_pnl
         [[ -z $limit_pnl ]] && limit_pnl="1"
         echo ""
-        echo -ne "Your name store [dafult: givpn-STORE] : "
+        echo -ne "Your name store [dafult: driwwvpn] : "
         read store_pnl
-        [[ -z $store_pnl ]] && store_pnl="givpn-STORE"
+        [[ -z $store_pnl ]] && store_pnl="driwwvpn"
         echo ""
 cat <<-EOF >/etc/.maAsiss/bot.conf
 admin_panel : $admin_pnl
@@ -139,15 +139,15 @@ EOF
         echo -e "Info...\n"
         fun_bot1() {
             [[ ! -e "/etc/.maAsiss/.Shellbtsss" ]] && {
-				wget -qO- https://raw.githubusercontent.com/givpn/bot_panel/master/ShellBot.sh >/etc/.maAsiss/.Shellbtsss
+				wget -qO- https://raw.githubusercontent.com/driwwvpn/bot_panel/master/ShellBot.sh >/etc/.maAsiss/.Shellbtsss
 			}
-			[[ "$(grep -wc "givpn_bot" "/etc/rc.local")" = '0' ]] && {
-			    sed -i '$ i\screen -dmS givpn_bot bbt' /etc/rc.local >/dev/null 2>&1
+			[[ "$(grep -wc "driww_bot" "/etc/rc.local")" = '0' ]] && {
+			    sed -i '$ i\screen -dmS driww_bot bbt' /etc/rc.local >/dev/null 2>&1
 			}
         }
         screen -dmS givpn_bot bbt >/dev/null 2>&1
         fun_bot1
-        [[ $(ps x | grep "givpn_bot" | grep -v grep | wc -l) != '0' ]] && echo -e "\nBot successfully activated !" || echo -e "\nError1! Information not valid !"
+        [[ $(ps x | grep "driww_bot" | grep -v grep | wc -l) != '0' ]] && echo -e "\nBot successfully activated !" || echo -e "\nError1! Information not valid !"
         sleep 2
         menu
         } || {
@@ -162,7 +162,7 @@ EOF
             sleep 1
         }
         fun_bot2
-        echo -e "\nBot givpn Stopped!"
+        echo -e "\nBot driwwvpn Stopped!"
         sleep 2
         menu
     }
